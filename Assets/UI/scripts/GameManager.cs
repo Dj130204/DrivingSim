c596a57d6c5c10fb8d31785dccfb411a925349c0
Dj130204 <bi23ut@student.sunderland.ac.uk>
2023-11-07 02:35:34 +0000

    timer
    Some post processing timer just needs adding to start/ Finish

---
+++ Assets/UI/scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // DESIGN PATTERN: SINGLETON
+    public static GameManager Instance { get; private set; }
+    public static Timer GameTimer { get; private set; }
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+
+        GameTimer = GetComponent<Timer>();
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Home");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
+
+}
